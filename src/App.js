c14af0deeb69899503e6f8ec7dd1d56bd8134db8
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2022-10-12 18:49:22 +0530

    setting bars

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import Bar from './components/Bar';
+class App extends Component {
+  state = {
+    array : [],
+    arraySteps: [],
+    colorKey: [],
+    colorSteps: [],
+    currentStep: 0,
+    count : 10,
+    delay : 100,
+    algorithm: '',
+    timeouts: [],    
+  } 
+  componentDidMount(){
+    this.generateRandomArray();
+  }
+  generateRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min)
+  }
+  generateRandomArray(){
+    const count = this.state.count; 
+    const temp = [];
+    for(let i = 0; i < count; i++){
+      temp.push(this.generateRandomNumber(50, 200));
+    }
+    this.setState({
+      array: temp,
+      arraySteps: [temp],
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    })
+  }
+  render() { 
+    let bars = this.state.array.map((value, index) => (
+      <Bar key={index} index={index} length={value} color={this.state.colorKey[index]}/>
+    ))
+    return <div className="app">
+      {bars}
+    </div>;
+  }
 }
-
+ 
 export default App;
